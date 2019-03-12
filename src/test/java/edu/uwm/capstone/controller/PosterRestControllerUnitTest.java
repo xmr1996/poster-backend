6dd67ddee65fd7e4223b7a3f8a9dfb7c3a211431
Fawzieh <fawziehassad@gmail.com>
2019-03-12 01:24:40 -0600

    Poster controller unit tests

--- src/test/java/edu/uwm/capstone/controller/PosterRestControllerUnitTest.java
+++ src/test/java/edu/uwm/capstone/controller/PosterRestControllerUnitTest.java
@@ -3,6 +3,7 @@ package edu.uwm.capstone.controller;
 
 import edu.uwm.capstone.db.PosterDao;
 import edu.uwm.capstone.model.Poster.Poster;
+import edu.uwm.capstone.sql.exception.DaoException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -53,14 +54,78 @@ public class PosterRestControllerUnitTest {
         verify(response, times(1)).sendError(HttpServletResponse.SC_PRECONDITION_FAILED, "Received null Poster object");
     }
 
-//    @Test
-//    public void createPreconditionFailed() throws IOException {
-//        Poster posterToCreate = posterWithTestValues();
-//        posterToCreate.setId(randomLong());
-//        posterRestController.create(posterToCreate, response);
-//
-//        verify(response, times(1)).sendError(HttpServletResponse.SC_PRECONDITION_FAILED, "Poster ID must be null");
-//    }
+
+    @Test
+    public void createException() throws IOException {
+        Poster posterToCreate = posterWithTestValues();
+        when(posterDao.create(any(Poster.class))).thenThrow(new DaoException(TEST_ERROR_MESSAGE));
+
+        posterRestController.create(posterToCreate, response);
+        verify(response, times(1)).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, TEST_ERROR_MESSAGE);
+    }
+
+    @Test
+    public void update() throws IOException {
+        Poster posterToUpdate = posterWithTestValues();
+        posterToUpdate.setId(randomLong());
+        when(posterDao.read(anyLong())).thenReturn(posterToUpdate);
+
+        posterRestController.update(posterToUpdate, response);
+        verify(posterDao, times(1)).update(posterToUpdate);
+    }
+
+    @Test
+    public void updateException() throws IOException {
+        Poster posterToUpdate = posterWithTestValues();
+        posterToUpdate.setId(randomLong());
+        when(posterDao.read(anyLong())).thenReturn(posterToUpdate);
+        doThrow(new DaoException(TEST_ERROR_MESSAGE)).when(posterDao).update(any(Poster.class));
+
+        posterRestController.update(posterToUpdate, response);
+        verify(response, times(1)).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, TEST_ERROR_MESSAGE);
+    }
+
+    @Test
+    public void updatePreconditionFailed() throws IOException {
+        posterRestController.update(posterWithTestValues(), response);
+        verify(response, times(1)).sendError(HttpServletResponse.SC_PRECONDITION_FAILED, "Poster Id must not be null");
+    }
+
+    @Test
+    public void readById() throws IOException {
+        Poster poster = posterWithTestValues();
+        poster.setId(randomLong());
+        when(posterDao.read(anyLong())).thenReturn(poster);
+
+        Poster receivedPoster = posterRestController.readById(poster.getId(), response);
+        assertEquals(poster, receivedPoster);
+    }
+
+    @Test
+    public void readByIdNotFound() throws IOException {
+        Long posterId = randomLong();
+        when(posterDao.read(anyLong())).thenReturn(null);
+
+        posterRestController.readById(posterId, response);
+        verify(response, times(1)).sendError(HttpServletResponse.SC_NOT_FOUND, "Poster with ID: " + posterId + " not found.");
+    }
+
+    @Test
+    public void delete() throws IOException {
+        Long posterId = randomLong();
+        posterRestController.delete(posterId, response);
+
+        verify(posterDao, times(1)).delete(posterId);
+    }
+
+    @Test
+    public void deleteIdNotFound() throws IOException {
+        Long posterId = randomLong();
+        doThrow(new DaoException(TEST_ERROR_MESSAGE)).when(posterDao).delete(anyLong());
+
+        posterRestController.delete(posterId, response);
+        verify(response, times(1)).sendError(HttpServletResponse.SC_NOT_FOUND, TEST_ERROR_MESSAGE);
+    }
 
 
 
