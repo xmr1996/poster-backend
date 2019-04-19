4d71cb3850add71f9ee9ba50b4dd4b88882e3f0d
Fawzieh <fawziehassad@gmail.com>
2019-04-19 04:31:34 -0500

    added some tests to score and round controllers

--- src/test/java/edu/uwm/capstone/controller/ScoreRestControllerUnitTest.java
+++ src/test/java/edu/uwm/capstone/controller/ScoreRestControllerUnitTest.java
@@ -1,8 +1,13 @@
 package edu.uwm.capstone.controller;
 
+import edu.uwm.capstone.db.JudgeDao;
+import edu.uwm.capstone.db.PosterDao;
 import edu.uwm.capstone.db.ScoreDao;
+import edu.uwm.capstone.model.Judge.Judge;
+import edu.uwm.capstone.model.Poster.Poster;
 import edu.uwm.capstone.model.Score.Score;
 import edu.uwm.capstone.sql.exception.DaoException;
+import edu.uwm.capstone.util.TestDataUtility;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -11,6 +16,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static edu.uwm.capstone.util.TestDataUtility.*;
 import static org.junit.Assert.assertEquals;
@@ -27,10 +34,17 @@ public class ScoreRestControllerUnitTest{
     private ScoreDao scoreDao;
 
     @Mock
+    private PosterDao posterDao;
+
+    @Mock
+    private JudgeDao judgeDao;
+
+    @Mock
     private HttpServletResponse response;
 
     @InjectMocks
     private ScoreRestController scoreRestController;
+
 
     private static final String TEST_ERROR_MESSAGE = randomAlphabetic(15);
 
@@ -48,10 +62,49 @@ public class ScoreRestControllerUnitTest{
         scoreRestController.create(null,response);
         verify(response, times(1)).sendError(HttpServletResponse.SC_PRECONDITION_FAILED,"Received null Score object");
     }
-//
-//    @Test
-//    public void createException() throws IOException{
-//        Score scoreToCreate = scoreWithTestValues();
-//    }
+
+    @Test
+    public void createException() throws IOException{
+        Score scoreToCreate = scoreWithTestValues();
+        when(scoreDao.create(any(Score.class))).thenThrow(new DaoException(TEST_ERROR_MESSAGE));
+
+        scoreRestController.create(scoreToCreate,response);
+        verify(response,times(1)).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, TEST_ERROR_MESSAGE);
+    }
+
+    @Test
+    public void GenerateRoundTwoAssignments() throws IOException{
+        List<String> posters = new ArrayList<>();
+        Poster poster1 = posterWithTestValues();
+        posters.add(poster1.getPoster_id());
+        when(posterDao.read(anyString())).thenReturn(poster1);
+
+        List<Judge> judges = new ArrayList<>();
+        Judge judge1 = TestDataUtility.judgeWithTestValues();
+        judges.add(judge1);
+        when(judgeDao.readAllJudges(poster1.getStatus())).thenReturn(judges);
+
+        Score score = scoreWithTestValues();
+        when(scoreDao.create(any(Score.class))).thenReturn(score);
+
+        scoreRestController.GenerateRoundTwoAssignments(posters,response);
+
+        //assertEquals(score.getPoster_id(),poster1.getPoster_id());
+        //assertEquals(score.getJudge_id(),judge1.getJudge_id());
+
+    }
+
+    @Test
+    public void GenerateRoundTwoAssignmentsException() throws IOException{
+        List<String> posters = new ArrayList<>();
+        Score score = scoreWithTestValues();
+        Poster poster = posterWithTestValues();
+        posters.add(poster.getPoster_id());
+        //when(posterDao.read(anyString())).thenReturn(poster);
+        //doThrow(new DaoException(TEST_ERROR_MESSAGE)).when(posterDao).read(anyString());
+        //scoreRestController.GenerateRoundTwoAssignments(posters,response);
+//        verify(response,times(1)).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, TEST_ERROR_MESSAGE);
+
+    }
 
 }
